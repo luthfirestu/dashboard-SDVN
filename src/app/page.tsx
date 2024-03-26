ed515a565c2b73bd86d773762463280be1ef8464
luthfirestu <luthfirestu05@gmail.com>
2024-03-26 14:27:10 +0700

    fix configuration form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,12 @@ export default function Home() {
         src="vector.svg" 
         alt="vector image" 
       />
-      <div className="m-12 w-1/2 space-y-4">
-        <h1 className="text-accent bold text-5xl">SDVN Testbed</h1>
+      <div className="m-12 w-1/2 space-y-4 absolute top-1/3">
+        <h1 className="text-accent font-bold text-5xl">SDVN Testbed</h1>
         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, to jdicbebacibceuiebcibc</p>
         <button onClick={() => router.push("/simulation/history")} className="btn btn-accent mt-4 bg-opacity-75">Get Started</button>
       </div>
+      <img src="self-driving-car.svg" alt="self-driving-car image" className="absolute bottom-0 right-0 mr-12 z-0 w-1/2 w-96" />
     </main>
   );
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,48 +1,77 @@
+import { useState } from 'react';
+
 export default function Form() {
+    const [power, setPower] = useState(40); // Initial power value
+
+    const handlePowerChange = (event) => {
+        setPower(event.target.value); // Update the power value when the input changes
+    };
+
     return (
-        <div className="col-2 w-1/2">
+        <div className="col-2">
             <div className="space-y-4">
-                <p className="weight-bold">General</p>
-                <div className="flex items-center gap-8"> {/* Flex container for Simulation Name */}
-                    <span className="w-1/3">Simulation Name</span> {/* Text */}
-                    <label className="input input-bordered flex items-center w-2/3"> {/* Input field */}
-                        <input type="text" placeholder="Example: 10 Cars in Manhattan" className="grow" />
-                    </label>
+                <div className="flex gap-12">
+                    <div className="space-y-3">
+                        <p className="font-bold">General</p>
+                        <div className="flex items-center gap-8"> {/* Flex container for Simulation Name */}
+                            <span className="w-1/3">Simulation Name</span> {/* Text */}
+                            <label className="input input-bordered flex items-center w-2/3"> {/* Input field */}
+                                <input type="text" placeholder="Example: 10 Cars in Manhattan" className="grow" />
+                            </label>
+                        </div>
+                        <div className="flex items-center gap-8"> {/* Flex container for Number of Cars */}
+                            <span className="w-1/3">Number of Cars</span> {/* Text */}
+                            <label className="input input-bordered flex items-center w-2/3 max-w-1/2"> {/* Input field */}
+                                <input type="text" className="grow" placeholder="daisy@site.com" />
+                            </label>
+                        </div>
+                        <div className="flex items-center gap-8"> {/* Flex container for Topology */}
+                            <span className="w-1/3">Topology</span> {/* Text */}
+                            <select className="select select-bordered w-2/3 max-w-1/2">
+                                <option disabled selected>Select Topology</option>
+                                <option>Bidirectional Highway 4 Plane</option>
+                                <option>Bidirectional Highway 6 Plane</option>
+                            </select>
+                        </div>
+                        <div className="flex items-center gap-8"> {/* Flex container for Simulation Duration */}
+                            <span className="w-1/3">Simulation Duration (ms)</span> {/* Text */}
+                            <label className="input input-bordered flex items-center w-2/3"> {/* Input field */}
+                                <input type="text" className="grow" placeholder="ex: 300" />
+                            </label>
+                        </div>
+                        <div className="flex items-center gap-8"> {/* Flex container for Maximum Vehicle Speed */}
+                            <span className="w-1/3">Maximum Vehicle Speed (km/h)</span> {/* Text */}
+                            <label className="input input-bordered flex items-center w-2/3"> {/* Input field */}
+                                <input type="text" className="grow" placeholder="ex: 80" />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="space-y-3">
+                        <p className="font-bold">Routing</p>
+                        <div className="flex items-center gap-8"> {/* Flex container for Routing Protocols */}
+                            <span className="w-1/3">Routing Protocols</span> {/* Text */}
+                            <select className="select select-bordered w-2/3">
+                                <option disabled selected>Select Routing Protocols</option>
+                                <option>Open Flow v1</option>
+                                <option>Adhoc Mesh</option>
+                            </select>
+                        </div>
+                        <p className='font-bold'>Power Adjustment</p>
+                        <div className="flex items-center gap-8"> {/* Flex container for Power Adjustment */}
+                            <span className="w-1/3">Threshold</span> {/* Text */}
+                            <input 
+                                type="range" 
+                                min={0} 
+                                max={100} 
+                                value={power} 
+                                className="range"
+                                onChange={handlePowerChange} // Handle range input change
+                            />
+                            <span>{power}</span> {/* Display current power value */}
+                        </div>
+                    </div>
                 </div>
-                <div className="flex items-center gap-8"> {/* Flex container for Number of Cars */}
-                    <span className="w-1/3">Number of Cars</span> {/* Text */}
-                    <label className="input input-bordered flex items-center w-2/3"> {/* Input field */}
-                        <input type="text" className="grow" placeholder="daisy@site.com" />
-                    </label>
-                </div>
-                <div className="flex items-center gap-8"> {/* Flex container for Topology */}
-                    <span className="w-1/3">Topology</span> {/* Text */}
-                    <select className="select select-bordered w-2/3 max-w-1/2">
-                        <option disabled selected>Select Topology</option>
-                        <option>Bidirectional Highway 4 Plane</option>
-                        <option>Bidirectional Highway 6 Plane</option>
-                    </select>
-                </div>
-                <div className="flex items-center gap-8"> {/* Flex container for Routing Protocols */}
-                    <span className="w-1/3">Routing Protocols</span> {/* Text */}
-                    <select className="select select-bordered w-2/3 max-w-1/2">
-                        <option disabled selected>Select Routing Protocols</option>
-                        <option>Open Flow v1</option>
-                        <option>Adhoc Mesh</option>
-                    </select>
-                </div>
-                <div className="flex items-center gap-8"> {/* Flex container for Simulation Duration */}
-                    <span className="w-1/3">Simulation Duration (ms)</span> {/* Text */}
-                    <label className="input input-bordered flex items-center w-2/3"> {/* Input field */}
-                        <input type="text" className="grow" placeholder="ex: 300" />
-                    </label>
-                </div>
-                <div className="flex items-center gap-8"> {/* Flex container for Maximum Vehicle Speed */}
-                    <span className="w-1/3">Maximum Vehicle Speed (km/h)</span> {/* Text */}
-                    <label className="input input-bordered flex items-center w-2/3"> {/* Input field */}
-                        <input type="text" className="grow" placeholder="ex: 80" />
-                    </label>
-                </div>
+                {/* Simulate button */}
                 <button className="btn btn-accent">Simulate</button>
             </div>
         </div>
